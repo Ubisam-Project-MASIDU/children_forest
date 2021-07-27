97300344d3efe2eafbd814f274d8ef16aadbf4be
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-27 15:13:32 +0900

    Jack Epi 8 & 13 & 14 & 15 code commenting

--- Assets/FairytaleStage/Jack/Jack_Epi14/Scripts/Blink.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi14/Scripts/Blink.cs
@@ -3,36 +3,46 @@
   * - Writer : 이윤교
   * - Content : 잭과콩나무 에피소드14 - 화살표 깜빡이는 효과 스크립트
   * 
-  *            -작성 기록-
-  *            2021-07-15 : 제작 완료
+  * - HISTORY
+  * 2021-07-15 : 초기 개발
+  * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
+  * 2021-07-27 : 주석 처리 수정
   *
+  * <Variable>
+  * mf_time : 깜빡거리는 속도
+  * mf_timer : 경과시간
+  * mf_waitingTime : 원하는 특정 시간 지정
   *
-  *            
   */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 미션 화살표 깜빡이게 하는 애니메이션 클래스
 public class Blink : MonoBehaviour{
-    float mf_time; //깜빡거리는 속도
-    float mf_timer;//현재 시간
-    float mf_waitingTime; //원하는 시간 지정
+    float mf_time;
+    float mf_timer;
+    float mf_waitingTime;
+    
+    //초기설정
     void Start(){
         mf_timer = 0.0f;
-        mf_waitingTime = 5f;
+        mf_waitingTime = 5.0f; //5초 뒤
     }
-    public void Update()
-    {
+
+    /*깜빡깜빡 거리는 효과*/
+    public void Update(){
         mf_timer += Time.deltaTime;
         if(mf_timer > mf_waitingTime){
             if (mf_time < 0.3f){
-                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1); //투명도 1
             }
             else{
-                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
-                if (mf_time > 1f)
+                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0); //투명도 0
+                if (mf_time > 1f){
                     mf_time = 0;
+                }
             }
             mf_time += Time.deltaTime;
         }
--- Assets/FairytaleStage/Jack/Jack_Epi15/Scripts/ChangeScene.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi15/Scripts/ChangeScene.cs
@@ -1,17 +1,34 @@
+/*
+ * - Name : ChangeScene.cs
+ * - Writer : 이윤교
+ * - Content : 잭과콩나무 에피소드15 - 잭과콩나무 동화 다시 듣기 버튼과 게임선택 씬으로 이동하는 버튼 스크립트
+ * 
+ * - HISTORY
+ * 2021-07-20 : 초기 개발
+ * 2021-07-27 : 주석 처리 수정
+ *
+ * <Function>
+ * ReStart() : Jack_Epi1 씬 로드 함수 (동화 다시 재생 버튼)
+ * Select() : select_stage 씬 로드 함수 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class ChangeScene : MonoBehaviour
-{
+
+//잭과콩나무 동화 다시 듣기 버튼과 게임선택 씬으로 이동하는 버튼 스크립트
+public class ChangeScene : MonoBehaviour{
+    //Jack_Epi1 씬 로드 함수 (동화 다시 재생 버튼)
     public void ReStart(){
         SceneManager.LoadScene("Jack_Epi1");
     }
 
+    //select_stage 씬 로드 함수 
     public void Select(){
         SceneManager.LoadScene("select_stage_scene");
-        var obj = GameObject.Find("BGMmanager");
-        if(obj != null) {
+        var obj = GameObject.Find("BGMmanager"); 
+        if(obj != null) { //BGM 끄기
             Destroy(obj);
         }
     }
