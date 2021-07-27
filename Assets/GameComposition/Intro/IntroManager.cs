4bcbbb0c5073521b27843d8c3b955b09310fc336
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-27 16:07:42 +0900

    add code comment

--- Assets/GameComposition/Intro/IntroManager.cs
+++ Assets/GameComposition/Intro/IntroManager.cs
@@ -3,11 +3,18 @@
   * - Writer : 이윤교
   * - Content : 게임 Intro 화면 구성
   * 
-  *            -작성 기록-
-  *            2021-07-19 : 제작 완료
+  * - HISTORY
+  * 2021-07-29 : 초기 개발 및 주석처리
+  * 2021-07-27 : 주석처리 수정
   *
-  *  IEnumerator DelayTime(float time) : 특정 시간 뒤에 함수 호출하기
+  * <Variable>
+  * mg_StartPanel : 게임시작화면
+  * mg_IntroPanel : 회사로고화면
+  *
+  * <Function>
+  * IEnumerator DelayTime(float time) : 특정 시간 뒤에 함수 호출하기
   *         -> StartCoroutine(DelayTime(4)); : IEnumerator 인터페이스를 사용하는 메소드를 실행하기 위해 특정 호출 메소드 이용
+  * GoStartSelectScene() : 스테이지 선택 화면으로 넘어감
   */
 
 using System.Collections;
@@ -15,25 +22,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class IntroManager : MonoBehaviour
-{
-    public GameObject g_StartPanel; //게임시작화면
-    public GameObject g_IntroPanel; //회사로고화면
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(DelayTime(4)); //4초 후 실행
+public class IntroManager : MonoBehaviour{
+    public GameObject mg_StartPanel;
+    public GameObject mg_IntroPanel;
+
+    //초기설정
+    void Start(){
+        StartCoroutine(DelayTime(4));               //4초 후 실행
     }
 
-    IEnumerator DelayTime(float time)
-    {
+    IEnumerator DelayTime(float time){
         yield return new WaitForSeconds(time);
-        g_IntroPanel.SetActive(false); //인트로화면 페이드 아웃
-        g_StartPanel.SetActive(true); //게임시작화면 페이드 인
+        mg_IntroPanel.SetActive(false);             //인트로화면 페이드 아웃
+        mg_StartPanel.SetActive(true);              //게임시작화면 페이드 인
     }
 
-    public void GoStartSelectScene()
-    {
-        SceneManager.LoadScene("select_stage_scene"); //Start 버튼을 누르면 스테이지 선택 화면으로 넘어감
+    //Start 버튼을 누르면 스테이지 선택 화면으로 넘어감
+    public void GoStartSelectScene(){
+        SceneManager.LoadScene("select_stage_scene"); 
     }
 }
--- Assets/GameComposition/Success/Scripts/End_controlStage.cs
+++ Assets/GameComposition/Success/Scripts/End_controlStage.cs
@@ -1,14 +1,14 @@
 /*
-  * - Name : Shape_Matching_Shape.cs
+  * - Name : End_controlStage.cs
   * - Writer : 이윤교
-  * - Content : 게임 성공 시 나타나는 씬, select스테이지 씬과 연결해줌
+  * - Content : select stage 씬과 연결해 주는 스크립트
   * 
-  *            -작성 기록-
-  *            2021-07-22 : 제작 완료 및 주석처리 완료
-  *            2021-07-22 : 주석처리 완료
+  * - HISTORY
+  * 2021-07-22 : 초기 개발 및 주석처리
+  * 2021-07-27 : 주석처리 수정
   *            
-  *
-  *
+  * <Function>
+  * v_changeSelectStage() : select_stage 씬 로드
   */
 
 using System.Collections;
@@ -16,13 +16,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class End_controlStage : MonoBehaviour
-{
+//select stage 씬으로 이동하는 클래스
+public class End_controlStage : MonoBehaviour{
     void Start() {
-        Invoke("v_changeSelectStage", 4f);
+        Invoke("v_changeSelectStage", 4f);            //4초 후 v_changeSelectStage 함수 실행
     }
-
     void v_changeSelectStage() {
-        SceneManager.LoadScene("select_stage_scene");
+        SceneManager.LoadScene("select_stage_scene"); //select_stage_scene 로드
     }
 }
