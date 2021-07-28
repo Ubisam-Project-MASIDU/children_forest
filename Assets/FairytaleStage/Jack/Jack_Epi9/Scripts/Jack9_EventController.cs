401c8543524073e4052153662d984695a6376904
kimmhyeon <kimmhyeon@gmail.com>
2021-07-28 10:27:07 +0900

    210728 잭과콩나무(에피소드9) - 스크립트 최적화 및 모듈화 작업

--- Assets/FairytaleStage/Jack/Jack_Epi9/Scripts/Jack9_EventController.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi9/Scripts/Jack9_EventController.cs
@@ -1,78 +1,43 @@
 ﻿/*
  * - Name : Jack9_EventController.cs
  * - Writer : 김명현
- * - Content : 잭과콩나무 에피소드9 - 이벤트 관리 스크립트
- *            게임진행 이벤트를 총괄적으로 관리하기 위한 스크립트
  * 
- *            
- *            
- *            
- *            
- *            -작성 기록-
- *            2021-07-14 : 제작 완료
- *            
- *            
- *            
+ * - Content :
+ * 잭과콩나무 에피소드9 - 이벤트 관리 스크립트
+ * 게임진행 이벤트를 총괄적으로 관리하기 위한 스크립트
  * 
- * -Variable 
+ * - Update Log -
+ * 1. 2021-07-14 : 제작 완료
+ * 2. 2021-07-16 : 화살표 기능 추가 및 인코딩형식 변경
+ * 3. 2021-07-23 : 음성기능 추가 및 주석 작성
+ * 4. 2021-07-27 : 중복되는 스크립트 정리
  * 
- * 게임 디렉터 오브젝트에 접근하기 위한 오브젝트
- * mg_ScriptManager
+ * - Variable 
+ * mg_GameDirector                  오브젝트 연결을 위한 변수 -> 게임 디렉터 오브젝트에 접근하기 위한 변수
+ * mg_Sack                          오브젝트 연결을 위한 변수 -> 자루 오브젝트에 접근하기 위한 변수
+ * mvm_playVoice                    오브젝트 연결을 위한 변수 -> VoiceManager 오브젝트에 접근하기 위한 변수
+ * mg_GenGiantSpeechBubble          말풍선 관련 변수 -> 거인 말풍선 관리를 위한 변수
+ * mg_GiantSpeech                   말풍선 관련 변수 -> Jack 말풍선 프리팹과 연결을 위한 변수
+ * mb_DontLoopEvent1                이벤트 관리를 위한 변수 -> 이벤트 반복을 피하기 위한 Flag
+ * mb_DontLoopEvent2                이벤트 관리를 위한 변수 -> 이벤트 반복을 피하기 위한 Flag
+ * mb_EventFlag                     이벤트 관리를 위한 변수 -> 이벤트 반복을 피하기 위한 Flag
+ * mn_EventSequence                 이벤트 관리를 위한 변수 -> 이벤트 순서를 관리하는 변수
+ * mb_SoundFlag                     이벤트 관리를 위한 변수 -> 처음 씬이 실행될때 음성이 한번만 나오기 위한 Flag
+ * mb_StopClickFlag                 이벤트 관리를 위한 변수 -> 클릭으로 다음 이벤트로 넘어가는 것을 방지하기위한 Flag
+ * IsSackDestroy                    이벤트 관리를 위한 변수 -> 자루가 파괴되었는지 확인하기위한 Flag
+ * mg_ArrowToSackLeft               화살표 관련 변수 -> 자루를 가르키는 왼쪽 화살표 변수
+ * mg_ArrowToSackRight              화살표 관련 변수 -> 자루를 가르키는 오른쪽 화살표 변수
+ * mg_ArrowPrefab                   화살표 관련 변수 -> 화살표 프리팹 연결을 위한 변수
  * 
- * 어머니 말풍선 관련 오브젝트
- * mg_GenMotherSpeechBubble
- * mg_MotherSpeech
- * 
- * 잭 말풍선 관련 오브젝트
- * mg_GenGiantSpeechBubble
- * mg_JackSpeech
- * 
- * 이벤트 관리 변수
- * mb_EventFlag : 이벤트를 한번만 작동하기 위한 flag
- * mn_EventSequence : 이벤트 순서를 관리하는 변수
- * 
- * 마우스 드래그 관련 오브젝트
- * mg_Bean
- * 
- * 마우스 클릭 제한 flag
- * StopClickFlag
- * 
- * 이벤트 성공확인을 위한 flag
- * mb_BeanToMother
- * mb_CowToGF
- * 
- * 
- * -Function
- * 
- * Flag 변경 함수
- * v_ChangeFlagFalse()
- * v_ChangeFlagTrue()
- * 
- * 메인 스크립트 함수
- * v_NextMainScript()
- * v_NoneMainScript()
- * 
- * 이벤트 스크립트 함수
- * v_NextEventScript()
- * v_NoneEventScript()
- * 
- * 잭 스크립트 함수
- * v_NextJackScript()
- * v_NoneJackScript()
- * 
- * 어머니 스크립트 함수
- * v_NextMotherScript()
- * v_NoneMotherScript()
- * 
- * 말풍선 생성 함수
- * v_GenJackSpeechBubble()
- * 
- * 말풍선 삭제 함수
- * v_RemoveJackSpeechBubble()
- * 
- * 
- * flag true 처리 함수
- * 
+ * - Function
+ * v_ChangeFlagFalse()              Flag 변경 함수 -> False로 설정
+ * v_ChangeFlagTrue()               Flag 변경 함수 -> True로 설정
+ * v_NextMainScript()               메인 스크립트 함수 -> 다음 메인 스크립트를 출력
+ * v_NoneMainScript()               메인 스크립트 함수 -> 메인 스크립트 내용을 지워 아무것도 출력안되게 설정
+ * v_NextEventScript()              이벤트 스크립트 함수 -> 다음 이벤트 스크립트를 출력
+ * v_NoneEventScript()              이벤트 스크립트 함수 -> 이벤트 스크립트 내용을 지워 아무것도 출력안되게 설정
+ * v_NextJackScript()               잭 스크립트 함수 -> 다음 Jack 스크립트를 출력
+ * v_NoneJackScript()               잭 스크립트 함수 -> Jack 스크립트 내용을 지워 아무것도 출력안되게 설정
  * 
  */
 
@@ -86,43 +51,39 @@ using UnityEngine.SceneManagement;
 
 public class Jack9_EventController : MonoBehaviour
 {
-    //게임 디렉터 오브젝트에 접근하기 위한 오브젝트
-    GameObject mg_ScriptManager;
-    GameObject mg_GenScript;
-    GameObject mg_Sack;
-    VoiceManager mvm_playVoice;
+    #region 변수 선언부
+    // 오브젝트 연결을 위한 변수
+    GameObject mg_GameDirector;                                                                         // 게임 디렉터 오브젝트에 접근하기 위한 변수
+    GameObject mg_Sack;                                                                                 // 자루 오브젝트에 접근하기 위한 변수
+    VoiceManager mvm_playVoice;                                                                         // VoiceManager 오브젝트에 접근하기 위한 변수
 
-    //잭 말풍선 관련 오브젝트
+    // 말풍선 관련 오브젝트
     GameObject mg_GenGiantSpeechBubble;
+    public GameObject mg_GiantSpeech;
 
     //이벤트 관리를 위한 변수
     private bool mb_DontLoopEvent1;
     private bool mb_DontLoopEvent2;
-    private bool mb_EventFlag;  //이벤트를 한번만 작동하기 위한 flag
-    private int mn_EventSequence;   //이벤트 순서를 관리하는 변수
-
-    public GameObject mg_GiantSpeech;
-    private bool mb_PlaySound;
-
-    //마우스 클릭 제한
-    private bool StopClickFlag;
-
-    //이벤트를위한 flag
+    private bool mb_EventFlag;                                                                          // 이벤트를 한번만 작동하기 위한 flag
+    private int mn_EventSequence;                                                                       // 이벤트 순서를 관리하는 변수
+    private bool mb_SoundFlag;
+    private bool StopClickFlag;                                                                         // 클릭으로 다음 이벤트로 넘어가는 것을 방지하기위한 Flag
     private bool IsSackDestroy;
 
     //화살표 오브젝트
-    GameObject mg_Arrow1;
-    GameObject mg_Arrow2;
+    GameObject mg_ArrowToSackLeft;
+    GameObject mg_ArrowToSackRight;
     public GameObject mg_ArrowPrefab;
-
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         //오브젝트 연결
-        this.mg_ScriptManager = GameObject.Find("GameDirector");
+        this.mg_GameDirector = GameObject.Find("GameDirector");
         this.mvm_playVoice = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
         this.mg_Sack = GameObject.Find("Sack");
+
         //이벤트 flag
         mb_DontLoopEvent1 = false;
         mb_DontLoopEvent2 = false;
@@ -133,17 +94,13 @@ public class Jack9_EventController : MonoBehaviour
         v_ChangeFlagFalse();
         mn_EventSequence = 0;
 
-
         //이벤트 시작
         v_NextMainScript();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
 
         if (Input.GetMouseButtonDown(0) && mvm_playVoice.mb_checkSceneReady)
         {
@@ -154,25 +111,25 @@ public class Jack9_EventController : MonoBehaviour
             v_ChangeFlagTrue();
         }
 
-        if (mn_EventSequence == 0 && mb_PlaySound == false && mvm_playVoice.mb_checkSceneReady)                    // 처음 씬이 실행되면 기본 스크립트 실행
+        if (mn_EventSequence == 0 && mb_SoundFlag == false && mvm_playVoice.mb_checkSceneReady)                    // 처음 씬이 실행되면 기본 스크립트 실행
         {
-            mb_PlaySound = true;
+            mb_SoundFlag = true;
             mvm_playVoice.playVoice(mn_EventSequence);
         }
-        else if (mn_EventSequence == 1 && this.mb_EventFlag == true && !(mvm_playVoice.isPlaying()) && mb_PlaySound == true)
+        else if (mn_EventSequence == 1 && this.mb_EventFlag == true && !(mvm_playVoice.isPlaying()) && mb_SoundFlag == true)
         {
             v_ChangeFlagFalse();
-            mb_PlaySound = false;
+            mb_SoundFlag = false;
             v_NoneMainScript();
 
             v_GenGiantSpeechBubble();
             v_NextGiantScript();
             mvm_playVoice.playVoice(mn_EventSequence);
         }
-        else if (mn_EventSequence == 2 && this.mb_EventFlag == true && mb_PlaySound == false)
+        else if (mn_EventSequence == 2 && this.mb_EventFlag == true && mb_SoundFlag == false)
         {
             v_ChangeFlagFalse();
-            mb_PlaySound = true;
+            mb_SoundFlag = true;
             v_RemoveGiantSpeechBubble();
 
             v_NextMainScript();
@@ -201,7 +158,7 @@ public class Jack9_EventController : MonoBehaviour
 
             v_NextMainScript();
 
-            this.mg_ScriptManager.GetComponent<Jack9_Gentreasure>().v_GenTreasure();
+            this.mg_GameDirector.GetComponent<Jack9_Gentreasure>().v_GenTreasure();
             mvm_playVoice.playVoice(mn_EventSequence+1);
             v_RemoveArrowToSack1();
             v_RemoveArrowToSack2();
@@ -216,7 +173,7 @@ public class Jack9_EventController : MonoBehaviour
         }
     }
 
-
+    #region 함수 선언부
     //Flag 변경 함수
     private void v_ChangeFlagFalse()
     {
@@ -230,31 +187,31 @@ public class Jack9_EventController : MonoBehaviour
     //메인 스크립트 함수
     private void v_NextMainScript()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_MainScript>().v_NextScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NextScript(0);
     }
     private void v_NoneMainScript()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_MainScript>().v_NoneScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NoneScript(0);
     }
 
     //이벤트 스크립트 함수
     private void v_NextEventScript()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_MissionScript>().v_NextScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NextScript(0);
     }
     private void v_NoneEventScript()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_MissionScript>().v_NoneScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NoneScript(0);
     }
     
     //거인 스크립트 함수
     private void v_NextGiantScript()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_GiantScript>().v_NextScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NextScript(1);
     }
     private void v_NoneGiantScript()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_GiantScript>().v_NoneScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NoneScript(1);
     }
     
     //말풍선 생성 함수
@@ -267,7 +224,7 @@ public class Jack9_EventController : MonoBehaviour
     //말풍선 삭제 함수
     private void v_RemoveGiantSpeechBubble()
     {
-        this.mg_ScriptManager.GetComponent<Jack9_GiantScript>().v_NoneScript();
+        this.mg_GameDirector.GetComponent<ScriptManager>().v_NoneScript(1);
         Destroy(this.mg_GenGiantSpeechBubble);
     }
     
@@ -278,34 +235,35 @@ public class Jack9_EventController : MonoBehaviour
 
     public void v_GenArrowToSack1()
     {
-        if (mg_Arrow1 == null)
+        if (mg_ArrowToSackLeft == null)
         {
-            mg_Arrow1 = Instantiate(mg_ArrowPrefab) as GameObject;
-            mg_Arrow1.transform.position = new Vector3(-0.87f, -0.57f, 0);
+            mg_ArrowToSackLeft = Instantiate(mg_ArrowPrefab) as GameObject;
+            mg_ArrowToSackLeft.transform.position = new Vector3(-0.87f, -0.57f, 0);
         }
     }
     public void v_GenArrowToSack2()
     {
-        if (mg_Arrow2 == null)
+        if (mg_ArrowToSackRight == null)
         {
-            mg_Arrow2 = Instantiate(mg_ArrowPrefab) as GameObject;
-            mg_Arrow2.transform.position = new Vector3(5, -0.57f, 0);
-            mg_Arrow2.GetComponent<SpriteRenderer>().flipX = true;
+            mg_ArrowToSackRight = Instantiate(mg_ArrowPrefab) as GameObject;
+            mg_ArrowToSackRight.transform.position = new Vector3(5, -0.57f, 0);
+            mg_ArrowToSackRight.GetComponent<SpriteRenderer>().flipX = true;
         }
     }
 
     public void v_RemoveArrowToSack1()
     {
-        if (mg_Arrow1 != null)
+        if (mg_ArrowToSackLeft != null)
         {
-            Destroy(mg_Arrow1);
+            Destroy(mg_ArrowToSackLeft);
         }
     }
     public void v_RemoveArrowToSack2()
     {
-        if (mg_Arrow2 != null)
+        if (mg_ArrowToSackRight != null)
         {
-            Destroy(mg_Arrow2);
+            Destroy(mg_ArrowToSackRight);
         }
     }
+    #endregion
 }
