5220b1dc37a74260fefead18d27e8b438063616c
choidj <69896751+choidj@users.noreply.github.com>
2021-07-29 10:45:21 +0900

    fix the drag bug..

--- Assets/GameComposition/Select/Scripts/main/HomeController.cs
+++ Assets/GameComposition/Select/Scripts/main/HomeController.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Main_backControl : MonoBehaviour {
-    public string ms_backScene = null;
+public class HomeController : MonoBehaviour
+{
     void Update() {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ra_checkMouseDistance = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D h_hitDistanceCast2D = Physics2D.GetRayIntersection(ra_checkMouseDistance, Mathf.Infinity);
 
-            if (h_hitDistanceCast2D.collider != null && h_hitDistanceCast2D.collider.name == "backController")
-            {
-                SceneManager.LoadScene(ms_backScene);
-            }
-            else if (h_hitDistanceCast2D.collider != null && h_hitDistanceCast2D.collider.name == "homeController")
+            if (h_hitDistanceCast2D.collider != null && h_hitDistanceCast2D.collider.name == "homeController")
             {
                 SceneManager.LoadScene("select_stage_scene");
-                var obj = GameObject.Find("BGMmanager");
+                var obj = GameObject.Find("BGMmanager");    
                 if(obj != null) {
                     Destroy(obj);
                 }
