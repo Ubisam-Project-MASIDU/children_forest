0de50d74de598d67fef6465616c8e7358a6e9f91
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-29 15:15:34 +0900

    버그 수정

--- Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/Drag_Jack.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/Drag_Jack.cs
@@ -8,15 +8,18 @@
  * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
  * 2021-07-22 : TTS 기능 추가
  * 2021-07-27 : 주석 처리 수정
+ * 2021-07-29 : 버그 수정
  *
  * <Variable>
  * mg_Jack : 잭 오브젝트
  * sc : 동화 스크립트 나타내기 위한 오브젝트
  * vm : 음성 TTS를 처리하는 오브젝트 연결
+ * DragFlag : Drag 처리
  *
  * <Function>
  * OnTriggerEnter2D(Collider2D cCollideObject) : 오브젝트간 충돌이 일어날때 처음 한번만 호출되는 함수
  * OnMouseDrag() : 게임오브젝트를 드래그로 이동시키는 함수
+ * ChangeDragFlagTrue() : 드래그 허용 함수
  * gotoEpi9Scene() : Epi9로 씬 이동을 위한 함수
  */
 
@@ -36,22 +39,15 @@ public class Drag_Jack : MonoBehaviour{
     void Start(){
         sc = ScriptControl.GetInstance();                                          // ScriptControl에서 Instance 리턴 받아 사용
         this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
-
-    }
-
-    void Update()
-    {
-
     }
 
     //오브젝트간 충돌이 일어날때 처음 한번만 호출되는 함수
     void OnTriggerEnter2D(Collider2D cCollideObject){                               //오브젝트간 충돌이 일어나면
-        OnMouseDrag();                                                              //드래그 허용
         if(cCollideObject.tag == "Closet"){                                         //Jack이 옷장 뒤에 숨으면
             sc.setNextScript();                                                     //다음 스크립트 제시
             vm.playVoice(1);                                                        //다음 스크립트에 해당하는 tts 실행
-            Destroy(this.gameObject);
-            Destroy(GameObject.Find("Click").gameObject);
+            Destroy(this.gameObject);                                               //잭 없애기
+            Destroy(GameObject.Find("Click").gameObject);                           //미션 화살표 없애기 
         }
         if(!vm.isPlaying()) {                                                       //tts재생이 끝나면
             Invoke("gotoEpi9Scene", 5f);                                            //5초 후 gotoEpi9Scene 함수 수행 
@@ -60,8 +56,7 @@ public class Drag_Jack : MonoBehaviour{
 
     //게임오브젝트를 드래그로 이동시키는 함수
     void OnMouseDrag(){
-        if(DragFlag == true)
-        {
+        if(DragFlag == true){                                                       //드래그 허용이 승인되면
             Vector2 v2mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             Vector2 v2worldObjPos = Camera.main.ScreenToWorldPoint(v2mousePosition);
             mg_Jack.transform.position = v2worldObjPos;
@@ -73,8 +68,8 @@ public class Drag_Jack : MonoBehaviour{
         SceneManager.LoadScene("Jack_Epi9");                                        //Jack_Epi9 씬 로드
     }
 
-    public void ChangeDragFlagTrue()
-    {
+    //드래그 허용 함수
+    public void ChangeDragFlagTrue(){
         DragFlag = true;
     }
 
--- Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/Movement_Giant.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/Movement_Giant.cs
@@ -8,10 +8,13 @@
   * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
   * 2021-07-22 : TTS 기능 추가
   * 2021-07-27 : 주석 처리 수정
+  * 2021-07-29 : 버그 수정
   *
   * <Variable>
   * mg_targetPosition : inspector창에서 walkPos 오브젝트로 지정해서 walkPos 위치로 거인 이동시킴
+  * mg_Jack : 잭
   * mb_checkPlayOnce : 스크립트 음성이 한번만 실행되게 하기 위한 체크하는 변수
+  * mb_JackFlag : Update 함수 안에서 Jack 한번만 찾도록 하는 변수
   * sc : 동화 스크립트 나타내기 위한 오브젝트
   * vm : 음성 TTS를 처리하는 오브젝트 연결
   *
@@ -26,18 +29,18 @@ using UnityEngine;
 //잭 뒤를 쫓도록 거인이 움직이는 클래스
 public class Movement_Giant : MonoBehaviour{
   public GameObject mg_targetPosition;
+  GameObject mg_Jack;
   bool mb_checkPlayOnce = false;
   public ScriptControl sc;
   VoiceManager vm;
-    GameObject mg_Jack;
-    bool Flag = false;
+  bool mb_JackFlag = false;
 
   //초기설정
   void Start(){
     sc = ScriptControl.GetInstance();
     this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
-        mg_Jack = GameObject.Find("Jack");
-    }
+    mg_Jack = GameObject.Find("Jack");
+  }
   
   void Update(){
     if(vm.mb_checkSceneReady){                                                                                             //tts 준비 작업이 다 끝났다면 
@@ -46,11 +49,10 @@ public class Movement_Giant : MonoBehaviour{
         vm.playVoice(0);                                                                                                   //스크립트 음성 재생
         mb_checkPlayOnce = true;                                                                                           //스크립트 음성 재생 체크
       }
-      else if (!vm.isPlaying() && Flag == false)
-            {
-                mg_Jack.GetComponent<Drag_Jack>().ChangeDragFlagTrue();
-                Flag = true;
-            }
+      else if (!vm.isPlaying() && mb_JackFlag == false){
+        mg_Jack.GetComponent<Drag_Jack>().ChangeDragFlagTrue();
+        mb_JackFlag = true;
+      }
     }
   }
 }
